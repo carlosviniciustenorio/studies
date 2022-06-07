2dbb559e501f80b692ab0a4ee3624467227f9879
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2022-06-06 23:13:15 -0300

    ajuste

--- Program.cs
+++ Program.cs
@@ -20,6 +20,10 @@ namespace studies
             var source = new BlockBlobClient(connectionString, "container1", "study.txt");
             var destination = new BlockBlobClient(connectionString, "container2", "study-copy.txt");
 
+            //token container 1 = sp=r&st=2022-06-07T01:50:03Z&se=2022-06-07T09:50:03Z&spr=https&sv=2020-08-04&sr=c&sig=ekN%2FabqgJrQdDVrK41rQAfnWhGWQrR8Yo4qZer7ouXI%3D
+            
+            azcopy copy 'https://<storagelabtenorio>.blob.core.windows.net/<container1>/<study.txt><sp=r&st=2022-06-07T01:50:03Z&se=2022-06-07T09:50:03Z&spr=https&sv=2020-08-04&sr=c&sig=ekN%2FabqgJrQdDVrK41rQAfnWhGWQrR8Yo4qZer7ouXI%3D>' 'https://<storagelabtenorio>.blob.core.windows.net/<container2>/<studyCMD.txt>'
+
             destination.StartCopyFromUri(source.Uri);
         }
     }
