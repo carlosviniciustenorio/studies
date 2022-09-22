041bb4d6e3ea37aaf57c2713029239afe681f8f6
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2022-09-21 21:26:44 -0300

    .

---
+++ Streams/Stream.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studies.Streams
+{
+    public class Stream
+    {
+        //Por intermédio das streams é possível transportar dados como voz, vídeo, imagem, entre outros.
+        //As streams suportam leituras e escritas síncronas e assíncronas.
+        //Classes para trabalhar com streams, sendo as principais:
+            //FileStream, MemoryStream, NetworkStream - derivadas de Stream, e BufferedStream que pode ser utilizada em conjunção com qualquer outra classe Stream.
+    }
+}
