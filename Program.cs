717aa9815857292417c0eb31da50ccd90d2d0ded
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2022-09-27 20:46:22 -0300

    tuples

--- Program.cs
+++ Program.cs
@@ -16,39 +16,13 @@ namespace studies
     {
         static void Main()
         {
-            var tuple = GetTuple();
-            Console.WriteLine($"Este é o valor da propriedade {nameof(tuple.n1)}: {tuple.n1}");
-            Console.WriteLine("--------------");
-            Console.WriteLine($"Este é o valor da propriedade {nameof(tuple.n2)}: {tuple.n2}");
-            Console.WriteLine("--------------");
-
-            //Desconstrução    
-            double n1;
-            double n2;
-
-            (n1, n2) = GetTuple();
-            Console.WriteLine($"Este é o valor da propriedade {nameof(n1)}: {n1}");
-            Console.WriteLine("--------------");
-            Console.WriteLine($"Este é o valor da propriedade {nameof(n2)}: {n2}");
-            Console.WriteLine("--------------");
-
-            //Desconstrução e Discard   
-            double n3;
-            double n4 = 0;
-
-            (n3, _) = GetTuple();
-            Console.WriteLine($"Este é o valor da propriedade {nameof(n3)}: {n3}");
-            Console.WriteLine("--------------");
-            Console.WriteLine($"Este é o valor da propriedade {nameof(n4)}: {n4}");
-            Console.WriteLine("--------------");
-        }
-
-        public static(double n1, double n2) GetTuple()
-        {
-            var number1 = 1;
-            var number2 = 2;
-
-            return (number1, number2);
+            // Create a 7-tuple.
+            var population = Tuple.Create("New York", 7891957, 7781984, 7894862, 7071639, 7322564, 8008278);
+            // Display the first and last elements.
+            Console.WriteLine("Population of {0} in 2000: {1:N0}",
+                            population.Item1, population.Item7);
+            // The example displays the following output:
+            //       Population of New York in 2000: 8,008,278
         }
     }
 }
--- Tuples/TupleTest.cs
+++ Tuples/TupleTest.cs
@@ -9,6 +9,13 @@ namespace studies.Tuples
     {
         // static void Main()
         // {
+        //    Create a 7-tuple.
+        // var population = Tuple.Create("New York", 7891957, 7781984, 7894862, 7071639, 7322564, 8008278);
+        // Display the first and last elements.
+        // Console.WriteLine("Population of {0} in 2000: {1:N0}",
+        //                 population.Item1, population.Item7);
+        // The example displays the following output:
+        //       Population of New York in 2000: 8,008,278
         //     var tuple = GetTuple();
         //     Console.WriteLine($"Este é o valor da propriedade {nameof(tuple.n1)}: {tuple.n1}");
         //     Console.WriteLine("--------------");
