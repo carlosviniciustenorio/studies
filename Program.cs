2690dc6ac058cec0f9f613755245e9a2757cb9ac
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2022-09-27 20:24:43 -0300

    tuple

--- Program.cs
+++ Program.cs
@@ -20,6 +20,27 @@ namespace studies
             Console.WriteLine($"Este é o valor da propriedade {nameof(tuple.n1)}: {tuple.n1}");
             Console.WriteLine("--------------");
             Console.WriteLine($"Este é o valor da propriedade {nameof(tuple.n2)}: {tuple.n2}");
+            Console.WriteLine("--------------");
+
+            //Desconstrução    
+            double n1;
+            double n2;
+
+            (n1, n2) = GetTuple();
+            Console.WriteLine($"Este é o valor da propriedade {nameof(n1)}: {n1}");
+            Console.WriteLine("--------------");
+            Console.WriteLine($"Este é o valor da propriedade {nameof(n2)}: {n2}");
+            Console.WriteLine("--------------");
+
+            //Desconstrução e Discard   
+            double n3;
+            double n4 = 0;
+
+            (n3, _) = GetTuple();
+            Console.WriteLine($"Este é o valor da propriedade {nameof(n3)}: {n3}");
+            Console.WriteLine("--------------");
+            Console.WriteLine($"Este é o valor da propriedade {nameof(n4)}: {n4}");
+            Console.WriteLine("--------------");
         }
 
         public static(double n1, double n2) GetTuple()
