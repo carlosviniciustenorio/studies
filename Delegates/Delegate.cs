7e71253e16402cd72ca41068ed5a058c8016a157
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2022-09-26 21:28:54 -0300

    how to use delegates c#

---
+++ Delegates/Delegate.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace studies.Delegates
+{
+    public class Delegate
+    {
+        // static void Main()
+        // {
+        //     Mensagem1(1);
+        //     Mensagem2(2);
+        //     Mensagem3(3);
+        // }
+
+        // static void Mensagem1(int n1){
+        //     string MinhaMensagem(int n1) => $"Mensagem {n1}";
+
+        //     var mensagemCompleta = ObterMensagem(MinhaMensagem, 1);
+        //     Console.WriteLine(mensagemCompleta);
+        // }
+        
+        // static void Mensagem2(int n2){
+        //     string MinhaMensagem(int n2) => $"Mensagem {n2}";
+
+        //     var mensagemCompleta = ObterMensagem(MinhaMensagem, 2);
+        //     Console.WriteLine(mensagemCompleta);
+        // }
+
+        // static void Mensagem3(int n3){
+        //     string MinhaMensagem(int n3) => $"Mensagem {n3}";
+
+        //     var mensagemCompleta = ObterMensagem(MinhaMensagem, n3);
+        //     Console.WriteLine(mensagemCompleta);
+        // }
+        
+        // public delegate string ObterMensagemEspecifica(int n1);
+
+        // static string ObterMensagem(ObterMensagemEspecifica mensagemEspecifica, int n1)
+        // {
+        //       string mensagem = string.Empty;
+        //       mensagem += "Inicio da mensagem\r\n\r\n";
+        //       mensagem += mensagemEspecifica(n1);
+        //       mensagem += "\r\n\r\n";
+        //       mensagem += "----------------------------\r\n";
+
+        //       return mensagem;
+        // }
+    }
+}
