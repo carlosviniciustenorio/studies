071158b39f3c481f948a81f58abdbcf96c17b838
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2022-10-18 10:18:49 -0300

    group by linq

---
+++ LINQ/LINQGroupBy.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace studies.LINQ
+{
+    public class LINQGroupBy
+    {
+        // static void Main()
+        // {
+        //     List<LinqGroupBy> list = new();
+        //     list.Add(new() {Id = 1, Nome = "Carlos"});
+        //     list.Add(new() {Id = 2, Nome = "Carol"});
+        //     list.Add(new() {Id = 3, Nome = "Isabelly"});
+        //     list.Add(new() {Id = 4, Nome = "Isabelly"});
+
+        //     var listGroupBy = list.GroupBy(d => d.Nome);
+
+        //     foreach (var itemGroupBy in listGroupBy)
+        //     {
+        //         List<LinqGroupBy> linqs = itemGroupBy.Select(d => d).ToList();
+
+        //         foreach (var linq in linqs)
+        //         {
+        //             Console.WriteLine($"It's {nameof(linq.Id)}: {linq.Id} and {nameof(linq.Nome)}:{linq.Nome}");
+        //             Console.ReadLine();
+        //         }
+        //     }
+        // }
+
+        // public class LinqGroupBy
+        // {
+        //     public int Id { get; set; }
+        //     public string Nome { get; set; }
+        // }
+    }
+}
