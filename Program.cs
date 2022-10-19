3ed4d210f5cdbe03a90f8c0c02dabd60d24d116b
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2022-10-19 08:30:55 -0300

    delegate operator

--- Program.cs
+++ Program.cs
@@ -7,11 +7,36 @@ namespace studies
     {
         static void Main()
         {
-            Console.WriteLine(EnumExpression.ToOrientation(Direction.Up));;
-            Console.WriteLine(EnumExpression.ToOrientation(Direction.Left));
-            Console.WriteLine(EnumExpression.ToOrientation(Direction.Down));
-            Console.WriteLine(EnumExpression.ToOrientation(Direction.Right));
-            Console.ReadLine();
+            Mensagem1(1);
+            Mensagem2(2);
+        }
+
+        static void Mensagem1(int n1){
+            // string MinhaMensagem(int n1) => $"Mensagem {n1}";
+            Func<int, string> MinhaMensagem = delegate (int n1) {return $"Mensagem {n1}";};
+            
+            var mensagemCompleta = ObterMensagem(MinhaMensagem, 1);
+            Console.WriteLine(mensagemCompleta);
+        }
+        
+        static void Mensagem2(int n2){
+            string MinhaMensagem(int n2) => $"Mensagem {n2}";
+
+            var mensagemCompleta = ObterMensagem(MinhaMensagem, 2);
+            Console.WriteLine(mensagemCompleta);
+        }
+        
+        public delegate string ObterMensagemEspecifica(Func<int, string> mensagemEspecifica);
+
+        static string ObterMensagem(Func<int, string> mensagemEspecifica, int n1)
+        {
+              string mensagem = string.Empty;
+              mensagem += "Inicio da mensagem\r\n\r\n";
+              mensagem += mensagemEspecifica(n1);
+              mensagem += "\r\n\r\n";
+              mensagem += "----------------------------\r\n";
+
+              return mensagem;
         }
     }
 }
