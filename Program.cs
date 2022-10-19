1da3c8af296e01c80c13c96e023bc9047d852ce4
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2022-10-19 08:11:42 -0300

    removing namespaces

--- Program.cs
+++ Program.cs
@@ -1,20 +1,5 @@
-﻿using Azure.Storage.Blobs.Specialized;
-using studies.Colecoes.Pilha;
-using studies.Genericos;
-using studies.Iterator;
-using studies.Record;
-using studies.Switch;
+﻿using studies.Switch;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-using static studies.Iterator.Class1;
 
 namespace studies
 {
